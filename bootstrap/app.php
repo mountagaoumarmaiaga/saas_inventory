c75bc4cc396c95d2fb81f983e301dbfa7dff9976
mountaga <mountagaoumarmaiga@gmail.com>
2026-02-22 20:27:13 +0000

    Trust all proxies to fix Koyeb HTTPS termination

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withMiddleware(function (Middleware $middleware): void {
+        // Confiance des proxys (pour Koyeb et l'HTTPS)
+        $middleware->trustProxies(at: '*');
+
         // Configuration des cookies
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 

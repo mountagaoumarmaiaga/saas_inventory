b3beb9683652c3601d1f9d61258a8168656b2b03
mountaga <mountagaoumarmaiga@gmail.com>
2026-03-06 12:13:06 +0000

    fix: resolve build failures - remove wayfinder plugin, fix ServiceProvider syntax, fix CSP nonces and storage URLs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,24 @@ use App\Models\Invoice;
 use App\Models\Payment;
 use App\Observers\InvoiceObserver;
 use App\Observers\PaymentObserver;
+use App\Repositories\Contracts\{
+    CategoryRepositoryInterface,
+    SubCategoryRepositoryInterface,
+    ProductRepositoryInterface,
+    ClientRepositoryInterface,
+    InvoiceRepositoryInterface,
+    StockMovementRepositoryInterface,
+    DeliveryNoteRepositoryInterface
+};
+use App\Repositories\{
+    CategoryRepository,
+    SubCategoryRepository,
+    ProductRepository,
+    ClientRepository,
+    InvoiceRepository,
+    StockMovementRepository,
+    DeliveryNoteRepository
+};
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +34,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(SubCategoryRepositoryInterface::class, SubCategoryRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
+        $this->app->bind(InvoiceRepositoryInterface::class, InvoiceRepository::class);
+        $this->app->bind(StockMovementRepositoryInterface::class, StockMovementRepository::class);
+        $this->app->bind(DeliveryNoteRepositoryInterface::class, DeliveryNoteRepository::class);
     }
 
     /**

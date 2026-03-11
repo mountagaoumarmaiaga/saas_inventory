2fecde00893095d9cb3d9f89ae939e0ed261e7ee
mountaga <mountagaoumarmaiga@gmail.com>
2026-03-11 17:48:24 +0000

    fix(axios): configure global instance to attach credentials and parse XSRF tokens to fix 419 errors on api calls

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -9,3 +9,5 @@ declare global {
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
